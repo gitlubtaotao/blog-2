8cdbf316fd5e2f9d541ae5fea8e9cec4eca943b8
hvkcoder <hvkcoder@gmail.com>
2019-12-24 22:49:29 +0800

    添加 @Controller 与 @RestController 注解的区别

---
+++ spring-practice/spring-boot/src/main/java/com/hvkcoder/springboot/config/ClusterDBSource.java
@@ -0,0 +1,9 @@
+package com.hvkcoder.springboot.config;
+
+/**
+ * @author h-vk
+ * @date 2019-12-24
+ * @since V1.0
+ */
+public class ClusterDBSource {
+}
--- spring-practice/spring-boot/src/main/java/com/hvkcoder/springboot/controller/IndexController.java
+++ spring-practice/spring-boot/src/main/java/com/hvkcoder/springboot/controller/IndexController.java
@@ -3,8 +3,10 @@ package com.hvkcoder.springboot.controller;
 import com.hvkcoder.springboot.domain.Department;
 import com.hvkcoder.springboot.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -12,17 +14,24 @@ import java.util.List;
  * @author h-vk
  * @date 2019-12-19
  * @since V1.0
+ *
+ * @RestController 相当于 @Controller + @ResponseBody
+ * @Controller 单独是使用，返回指定的视图
  */
-@RestController
+@Controller
 public class IndexController {
 
     @Autowired
     private DepartmentService departmentService;
 
     @RequestMapping("/")
-    public List<Department> index() {
-        return departmentService.findAllDepartment();
+    public String index() {
+        return "index";
     }
 
-
+    @GetMapping(path = "/getDepartmentAll")
+    @ResponseBody
+    public List<Department> getDepartmentAll() {
+        return departmentService.findAllDepartment();
+    }
 }
